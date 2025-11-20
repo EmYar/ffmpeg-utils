96018be5d655db5bccdb4cdcca40a37c17b50a8a
Yaroslav Emelyanov <emel.yar95@gmail.com>
2025-11-20 20:46:26 +0400

    - PARALLELISM ENV fix

--- src/main/kotlin/me/emyar/ffmpegutils/Routing.kt
+++ src/main/kotlin/me/emyar/ffmpegutils/Routing.kt
@@ -1,14 +1,17 @@
 package me.emyar.ffmpegutils
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.sync.Semaphore
 import me.emyar.ffmpegutils.processing.batch.filesBatchRoute
-import me.emyar.ffmpegutils.processing.common.PARALLELISM
+import me.emyar.ffmpegutils.processing.common.DEFAULT_PARALLELISM
 import me.emyar.ffmpegutils.processing.common.fileInfo
 import me.emyar.ffmpegutils.processing.single.singleFileRoute
 
-private val parallelismSemaphore = Semaphore(PARALLELISM)
+private val log = KotlinLogging.logger {}
+
+private val parallelismSemaphore = Semaphore(getParallelism())
 
 fun Application.configureAudioNormalizationRouting() {
     routing {
@@ -18,4 +21,15 @@ fun Application.configureAudioNormalizationRouting() {
             filesBatchRoute(parallelismSemaphore)
         }
     }
+}
+
+private fun getParallelism(): Int {
+    log.info { "Trying to read parallelism limit from PARALLELISM environment variable" }
+    val env = System.getenv("PARALLELISM")
+        ?: run {
+            log.info { "PARALLELISM is not set, using default parallelism = $DEFAULT_PARALLELISM" }
+            return DEFAULT_PARALLELISM
+        }
+    log.info { "Using parallelism = $env" }
+    return env.toInt()
 }
--- src/main/kotlin/me/emyar/ffmpegutils/processing/common/Constants.kt
+++ src/main/kotlin/me/emyar/ffmpegutils/processing/common/Constants.kt
@@ -1,25 +1,11 @@
 package me.emyar.ffmpegutils.processing.common
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import java.nio.file.Path
 import java.nio.file.Paths
 
-private val log = KotlinLogging.logger {}
-
 const val EBU_R128_CONFIG = "I=-23:TP=-2:LRA=7"
 
-val PARALLELISM = getParallelism()
+const val DEFAULT_PARALLELISM = 1
 
 val BASE_IN_PATH: Path = Paths.get(System.getenv("DEFAULT_INPUT_PATH_BASE")!!)
 val BASE_OUT_PATH: Path = Paths.get(System.getenv("DEFAULT_OUTPUT_PATH_BASE")!!)
-
-private fun getParallelism(): Int {
-    log.info { "Trying to read parallelism limit from PARALLELISM environment variable" }
-    val env = System.getenv("PARALLELISM")
-        ?: run {
-            log.info { "PARALLELISM is not set, using default parallelism = 1" }
-            return 1
-        }
-    log.info { "Using parallelism = $env" }
-    return env.toInt()
-}
