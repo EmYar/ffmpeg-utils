66423d827ad4cb6d596be7c77f0cb0b57291d28a
EmYar <emel.yar95@gmail.com>
2026-08-15 00:40:59 +0400

    dependencies upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
 //    implementation("io.ktor:ktor-server-request-validation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-server-routing-openapi:$ktorVersion")
     implementation("io.ktor:ktor-server-swagger:$ktorVersion")
 
@@ -107,6 +106,6 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = "9.6.1"
+        gradleVersion = "9.7.0"
     }
 }
